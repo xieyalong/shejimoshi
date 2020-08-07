8e68a8fd523868a07f2c184c32d3f7f26fb697ef
cjx <13269169250@163.com>
2020-08-07 16:21:08 +0800

    抽象工厂模式

--- sjms/src/com/xyl/模式/工厂模式/抽象工厂模式/抽象工厂模式.java
+++ sjms/src/com/xyl/模式/工厂模式/抽象工厂模式/抽象工厂模式.java
@@ -1,30 +1,57 @@
-package com.xyl.模式.工厂模式.工厂方法模式;
+package com.xyl.模式.工厂模式.抽象工厂模式;
 
-import java.time.Year;
 import java.util.Scanner;
 
-public class 工厂方法模式 {
+public class 抽象工厂模式 {
     //客户端
     public static void main(String[] args) {
-        //北京口味的各种披萨
-        DingDanFactory factory=  new BJDingDanFactory ();
-        factory.factory();
-        //伦敦口味的各种披萨
-        DingDanFactory factory2=  new LDDingDanFactory ();
-        factory2.factory();
+//        new DingDan().factory(new BjFactory());
+        new DingDan().factory(new LdFactory());
     }
 }
+//工厂接口类
+interface AbsFactory{
+    Pizz createPizz(String type);
+}
+//北京工厂类
+class BjFactory implements AbsFactory{
 
+    @Override
+    public Pizz createPizz(String type) {
+        Pizz pizz=null;
+        if ("1".equals(type)){
+            pizz=new BJLiuLianPizz();
+            pizz.name="北京榴莲披萨";
+        }else{
+            System.out.println("目前北京只有榴莲披萨");
+        }
+        return pizz;
+    }
+}
+//伦敦工厂类
+class LdFactory implements AbsFactory{
+
+    @Override
+    public Pizz createPizz(String type) {
+        Pizz pizz=null;
+        if ("2".equals(type)){
+            pizz=new LDLiuLianPizz();
+            pizz.name="伦敦榴莲披萨";
+        }else{
+            System.out.println("目前伦敦只有榴莲披萨");
+        }
+        return pizz;
+    }
+}
 //订单
-abstract class DingDanFactory{
-    //具体功能操作
-    public void factory() {
+class DingDan{
+    public void factory(AbsFactory absFactory) {
         String type=null;
         Pizz pizz=null;
         do {
             type=getType();
             //调用抽象方法 实现由子类来完成
-            pizz= cretePizz(type);
+            pizz=absFactory.createPizz(type);
             //输出披萨制作过程
             if (null!=pizz){
                 pizz.prepare();
@@ -37,45 +64,12 @@ abstract class DingDanFactory{
 
         }while (true);
     }
-    //定义一个工厂的抽象类 由工厂子类来完成
-    protected abstract Pizz cretePizz(String type);
-
     //获取用户输入的披萨种类
     private   String getType(){
         Scanner input =new Scanner(System.in);
         System.out.println("输入要订购的披萨种类 1是北京榴莲披萨 2是伦敦榴莲披萨");
         String name=input.nextLine();
         return name;
-    }
-}
-//北京订单榴莲披萨
-class BJDingDanFactory extends DingDanFactory{
-
-    @Override
-    protected Pizz cretePizz(String type) {
-        Pizz pizz=null;
-        if ("1".equals(type)){
-            pizz=new BJLiuLianPizz();
-            pizz.name="北京榴莲披萨";
-        }else{
-            System.out.println("目前北京只有榴莲披萨");
-        }
-        return pizz;
-    }
-}
-//伦敦订单榴莲披萨
-class LDDingDanFactory extends DingDanFactory{
-
-    @Override
-    protected Pizz cretePizz(String type) {
-        Pizz pizz=null;
-        if ("2".equals(type)){
-            pizz=new LDLiuLianPizz();
-            pizz.name="伦敦榴莲披萨";
-        }else{
-            System.out.println("目前伦敦只有榴莲披萨");
-        }
-        return pizz;
     }
 }
 //披萨基类 提供方
@@ -105,28 +99,27 @@ abstract class  Pizz{
     }
 }
 
-class BJLiuLianPizz extends Pizz{
+class BJLiuLianPizz extends Pizz {
     @Override
     public void prepare() {
         System.out.println("北京的榴莲披萨员材料");
     }
 }
-class BJZhiShiPizz extends Pizz{
+class BJZhiShiPizz extends Pizz {
     @Override
     public void prepare() {
         System.out.println("北京的芝士披萨员材料");
     }
 }
-class LDLiuLianPizz extends Pizz{
+class LDLiuLianPizz extends Pizz {
     @Override
     public void prepare() {
         System.out.println("伦敦的榴莲披萨员材料");
     }
 }
-class LDZhiShiPizz extends Pizz{
+class LDZhiShiPizz extends Pizz {
     @Override
     public void prepare() {
         System.out.println("伦敦的芝士披萨员材料");
     }
 }
-
